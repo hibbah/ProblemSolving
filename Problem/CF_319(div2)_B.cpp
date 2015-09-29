f6bb1f8e95e95c195a9b18998373f4c6937366b7
Hibbah <wjy721@naver.com>
2015-09-30 01:10:55 +0900

    CF319div2_B
    math(modulo), DP

---
+++ Problem/CF_319(div2)_B.cpp
@@ -0,0 +1,76 @@
+/**********************************************************************************
+
+2015.09.29
+http://codeforces.com/problemset/problem/577/B
+
+- title : Modulo Sum
+- input : N(<=1,000,000), M(<=1,000), arr[N](<= 1,000,000,000)
+- output : arr의 부분집합 중, 합을 M으로 나눈 결과가 0이 되는 부분집합이 존재하는가?
+- issue :
+	1. 부분집합의 수 == 2^N
+	2. dp[idx][R] : idx~ 의 조합으로 M으로 나눈 결과가 R이 되는 부분집합의 개수
+		=> dp[N][M] == 1,000,000,000 => 메모리&시간 초과
+
+- solution :
+	Sum[a][b] : arr[a]~arr[b]까지의 구간합으로 정의.
+	N > M인 경우, 비둘기집의 원리에 의해 Sum[0][i]==Sum[0][j]인 서로 다른 i,j쌍이 반드시 존재.
+	i < j, Sum[0][i] % M == Sum[0][j] % M == R 이라고 가정 하면,
+	Sum[0][j] = Sum[0][i] + Sum[i+1][j]에서
+	Sum[0][j] % M = ( Sum[0][i]%M + Sum[i+1][j]%M ) % M 이 되고,
+	R == ( R + Sum[i+1][j]%M ) % M 이므로, 반드시 Sum[i+1][j]%M == 0을 만족해야함.
+	따라서, 합을 M으로 나눈 결과가 0이 되는 부분집합 Set[i+1][j]가 반드시 존재함.
+
+	1. N > M인 경우, 무조건 "YES"를 출력
+	2. N <= M인 경우, dp[1000][1000] 과정 수행
+
+- complexity : O(M^2)
+
+**********************************************************************************/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+typedef unsigned long long ull;
+
+int n, m;
+int arr[1000001];
+ull dp[1001][1001];
+
+ull isPossible(const int idx, const int R)
+{
+	if (idx == n) return R == 0 ? 1 : 0;
+
+	ull & ret = dp[idx][R];
+	if (ret != -1) return ret;
+	ret = 0;
+
+	ret = isPossible(idx + 1, R);
+	ret += isPossible(idx + 1, (R + m - arr[idx]) % m);
+
+	return ret;
+}
+
+int main()
+{
+#ifdef _CONSOLE
+	freopen("input.txt", "r", stdin);
+#endif
+
+	memset(dp, -1, sizeof(dp));
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d", &arr[i]);
+		arr[i] %= m;
+	}
+	bool result;
+	if (n > m) result = true;
+	else result = isPossible(0, 0) >= 2 ? true : false;
+	
+	printf("%s\n", result ? "YES" : "NO");
+
+	return 0;
+}
+
